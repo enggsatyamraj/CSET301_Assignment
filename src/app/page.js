678e5e195c241f0ce3a8664f47ef3404d87b05bd
Satyaaam <sattu62025@gmail.com>
2024-02-02 17:42:57 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function page() {
     <div className="bg-normal w-[100%]">
       <div className="max-w-[1280px] mx-auto px-4 text-white relative min-h-[100vh] w-[100%] ">
         {/* Hero section */}
-        <div className="min-h-[90vh] border-2 relative flex flex-col pl-[20px] sm:pl-[50px] justify-center z-4">
+        <div className="min-h-[90vh] relative flex flex-col pl-[20px] sm:pl-[50px] justify-center z-4">
           <p className="pt-[60px]  text-5xl sm:text-6xl mb-10 md:text-7xl  sm:w-[80%] md:w-[70%] lh:w-[60%]">
             Buy <span className="span_text">Social Media</span> accounts at resonable prices.
           </p>
