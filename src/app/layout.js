f323d976d7d9bdf2bc3a722eefce912aada89d5d
boora-aman <aman.chaudharyboora@gmail.com>
2024-02-23 11:56:57 +0530

    Added Favicon icons and changes in layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,18 @@ const actor = Actor({ weight: "400", subsets: ["latin"] });
 export const metadata = {
   title: "Discord arena",
   description: "A place to find discord accounts and join discord servers, communities and events.",
+  icons: {
+    icon: [
+      '/favicon.ico?v=4',
+    ],
+    apple: [
+      '/apple-touch-icon.png?v=4',
+    ],
+    shortcut: [
+      '/favicon.ico?v=4',
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
